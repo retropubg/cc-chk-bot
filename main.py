68977d435067515f03dd46c9ddfb5d00c629d7c0
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-08 15:51:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ async def main():
                 api_id="24578407",
                 api_hash="5f711fbe013fd0d20147f62728118510")
   bot = Client("my_bot",
-               api_id="24578407",
-               api_hash="5f711fbe013fd0d20147f62728118510",
-               bot_token="6127557847:AAEGXigaZIS4MHoxOudREzxZnNQKtu4TjD8",
+               api_id="26043952",
+               api_hash="96b8dea447ef580b5b75b01ccc3ab710",
+               bot_token="7020048572:AAG5bV9yhIk4DVw3ynUo-j9GHS743f9xVyA",
                plugins=plugins)
   clients = [user, bot]
   bot.set_parse_mode(enums.ParseMode.HTML)
